0d0ebc77ebc200385c19a0e7d785d4ee4497999b
Aditya <ranjanaditya452@gmail.com>
2025-09-18 22:48:01 +0530

    Created homepage and its dashboard

--- stock-trackerFE/src/App.jsx
+++ stock-trackerFE/src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Route,Router,Routes } from 'react-router-dom'
+import Homepage from './components/Homepage'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Homepage/>}/>
+      </Routes>
+    </Router>
   )
 }
 
---
+++ stock-trackerFE/src/components/Homepage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Dashboard from './micro-components/Dashboard'
+
+const Homepage = () => {
+  return (
+    <Dashboard/>
+  )
+}
+
+export default Homepage
