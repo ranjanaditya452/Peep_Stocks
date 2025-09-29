873b2efe4c247069fca889c752868abf1ddaceb2
Aditya <ranjanaditya452@gmail.com>
2025-09-29 23:20:08 +0530

    Need to refactor and use grid in some areas, definitely lacking in exp

--- stock-trackerFE/src/components/micro-components/Navbar.jsx
+++ stock-trackerFE/src/components/micro-components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-  <div className='flex flex-col '>
-   <div className='flex py-3 pl-16 border-2 border-red-200'>
+  <div className='flex flex-col h-screen'>
+   <div className='flex py-3 pl-16 grow-none  border-2 border-red-200'>
      <div className='border-r-2 border-gray-300 pr-4'>Peep-Stocks</div>
      <div className='flex justify-between w-100 px-6 border-2 border-red-200 '>
       <div>Placeholder&nbsp;1</div>
@@ -15,8 +15,9 @@ const Navbar = () => {
       <div>Dashboard</div>
      </div>
    </div>
-   <div className='flex justify-center content-center grow  border-red-200'>
-    <h1 className='text-5xl  border-blue-500'>Track it. Scroll it. Save it.</h1>
+   <div className='flex flex-col grow items-center mt-30'>
+    <div className='text-5xl  '>Track it. Scroll it. Save it.</div>
+    <div className=''>The open platform to track your stocks</div>
    </div>
   </div>
     
