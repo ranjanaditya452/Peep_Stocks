52f01d5c01dbd35b67c04a945219453b282cdde8
Aditya <ranjanaditya452@gmail.com>
2025-09-29 22:54:49 +0530

    Homepage progress, making the center area

--- stock-trackerFE/src/components/micro-components/Navbar.jsx
+++ stock-trackerFE/src/components/micro-components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
+  <div className='flex flex-col '>
    <div className='flex py-3 pl-16 border-2 border-red-200'>
      <div className='border-r-2 border-gray-300 pr-4'>Peep-Stocks</div>
      <div className='flex justify-between w-100 px-6 border-2 border-red-200 '>
@@ -14,6 +15,10 @@ const Navbar = () => {
       <div>Dashboard</div>
      </div>
    </div>
+   <div className='flex justify-center content-center grow  border-red-200'>
+    <h1 className='text-5xl  border-blue-500'>Track it. Scroll it. Save it.</h1>
+   </div>
+  </div>
     
   )
 }
