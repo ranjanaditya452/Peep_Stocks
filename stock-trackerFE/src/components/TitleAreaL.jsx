671a949f43828e00edaa31017717e5946652e92b
Aditya <ranjanaditya452@gmail.com>
2025-10-03 02:44:49 +0530

    Done with searchbar

--- stock-trackerFE/src/components/TitleAreaL.jsx
+++ stock-trackerFE/src/components/TitleAreaL.jsx
@@ -1,21 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
+import SearchBar from './microcomponents/SearchBar'
 
 const TitleAreaL = () => {
-  const [searchQuery, setSearchQuery] = useState("");
+  
   return (
     <>
       <div className='flex flex-col gap-10'>
         <div className='text-8xl'>Track it. Scroll it. Save it.</div>
-        <div className='text-3xl'>The one platform to track your stocks</div>
-
-        <div className='w-3/4 relative'>
-          <input type="search"
-            placeholder="Eg. AAPL"
-            value={searchQuery}
-            onChange={(e)=>setSearchQuery(e.target.value)}
-            className='w-full px-6 text-xl rounded-full h-14 border-3 border-gray-300 focus:border-[rgb(223,111,149)] hover:border-[rgb(223,111,149)]
-            focus:outline-none bg-gray-100'/>
-        </div>
+        <div className='text-3xl'>The one platform to view all your stocks</div>
+        <SearchBar/>
       </div>
 
     </>
---
+++ stock-trackerFE/src/components/microcomponents/SearchBar.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import searchIcon from '../../assets/searchIcon.svg'
+
+const SearchBar = () => {
+    const [searchQuery, setSearchQuery] = useState("");
+  return (
+    <>
+    <div className='w-3/4 relative group'>
+          <input type="search"
+            placeholder="Eg. AAPL"
+            value={searchQuery}
+            onChange={(e)=>setSearchQuery(e.target.value)}
+            className='w-full pl-6 pr-15 text-2xl rounded-full h-14 border-3
+            border-gray-300 focus:border-[rgb(231,170,190)] group-hover:border-[rgb(231,170,190)]
+            focus:outline-none bg-gray-100'/>
+
+            <span className='absolute right-3 top-2 flex place-content-center 
+                  w-10 h-10 rounded-full bg-[rgb(223,111,149)] hover:bg-[rgb(227,134,165)] cursor-pointer'>
+                <img src={searchIcon} alt="SearchIcon" className='w-8 h-8 mt-1' /></span>
+        </div>
+    </>
+  )
+}
+
+export default SearchBar
