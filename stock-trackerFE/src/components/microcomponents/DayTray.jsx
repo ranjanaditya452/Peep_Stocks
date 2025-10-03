d0699c807db9c629d508de48107481e7900dbcb6
Aditya <ranjanaditya452@gmail.com>
2025-10-03 11:35:54 +0530

    Daytray done

--- stock-trackerFE/src/components/microcomponents/DayTray.jsx
+++ stock-trackerFE/src/components/microcomponents/DayTray.jsx
@@ -1,22 +1,25 @@
-import React, { useContext } from 'react'
-import { OutlineContext } from '../../context/OutlineContext'
-
 const DayTray = () => {
-    const outline = useContext(OutlineContext);
-    let buttonStyle ="flex items-center gap-2 px-3 py-2 rounded-full h-13 hover:cursor-pointer";
-  return (
-    <div className={`${outline} flex justify-between text-2xl w-3/4`} >
-        <button className={`${buttonStyle} bg-[rgb(33,32,32)]`}>
-            <img className="w-7 h-7" src="/intraday.svg" alt="day" />Intraday
-        </button>
-        <button className={`${buttonStyle}`}>
-            <img className="w-7 h-7" src="/weekly.svg" alt="week" />Weekly
-        </button>
-        <button className={`${buttonStyle}`}>
-            <img className="w-7 h-7" src="/monthly.svg" alt="month" />Monthly
-        </button>
-    </div>
-  )
-}
+  
+  const buttonStyle =
+    "flex items-center justify-center gap-2 px-8 py-2 rounded-full h-14 hover:cursor-pointer";
 
-export default DayTray
+  return (
+    <div className={` flex justify-between text-2xl w-3/4`}>
+      <button className={`${buttonStyle} text-white bg-[#3a3546] hover:bg-[#3f3d47]`}>
+        <img className="w-8 h-8 " src="/intraday.svg" alt="day" />
+        <span className="relative bottom-0.5">Intraday</span>
+      </button>
+      <button className={`${buttonStyle} hover:bg-[#faf9fb]`}>
+        <img className="w-8 h-8" src="/weekly.svg" alt="week" />
+        <span className="relative bottom-0.5">Weekly</span>
+        
+      </button>
+      <button className={`${buttonStyle} hover:bg-[#faf9fb]`}>
+        <img className="w-8 h-8" src="/monthly.svg" alt="month" />
+        <span className="relative bottom-0.5">Monthly</span>
+      </button>
+    </div>
+  );
+};
+
+export default DayTray;
