0a1285ad0a03c83fdccf253c0214f8dd2541c471
Aditya <ranjanaditya452@gmail.com>
2025-10-22 23:46:26 +0530

    readability in webclient

--- server/src/main/java/com/Adi/stock_tracker/config/WebClientConfig.java
+++ server/src/main/java/com/Adi/stock_tracker/config/WebClientConfig.java
@@ -15,7 +15,7 @@ public class WebClientConfig {
     @Bean
     public WebClient webClient(WebClient.Builder builder )
     {
-            return builder.baseUrl(alphaVantageUrl) .exchangeStrategies(ExchangeStrategies.builder()
+            return builder.baseUrl(alphaVantageUrl).exchangeStrategies(ExchangeStrategies.builder()
                             .codecs(configurer -> configurer
                                     .defaultCodecs()
                                     .maxInMemorySize(16 * 1024 * 1024))
