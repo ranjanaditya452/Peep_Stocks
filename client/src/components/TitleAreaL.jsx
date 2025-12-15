19a5420888a1b8ae6b026327c76843790e2a6c22
Aditya <ranjanaditya452@gmail.com>
2025-12-15 22:49:29 +0530

    Added explicit grid rows

--- client/src/components/TitleAreaL.jsx
+++ client/src/components/TitleAreaL.jsx
@@ -8,7 +8,7 @@ const TitleAreaL = () => {
   return (
     <>
       <div className='flex flex-col gap-10'>
-        <div className=' text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl max-w-[12ch] '>
+        <div className=' text-4xl sm:text-5xl md:text-6xl lg:text-7xl max-w-[12ch] '>
           Track it. Scroll it. Save it.</div>
         <div className='text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl'>The one platform to view all your stocks</div>
         <SearchBar/>
--- client/src/pages/Overview.jsx
+++ client/src/pages/Overview.jsx
@@ -15,17 +15,17 @@ const OverviewMain = () =>{
     const outlineClasses = useContext(OutlineContext);
     return (
       <div className={`grid grid-cols-[auto_1fr]
-                       md:grid-cols-[minmax(20px,140px)_1fr]
+                       md:grid-cols-[minmax(20px,140px)_1fr] md:grid-rows-2
                        justify-items-start ${outlineClasses}`}>
-        <div>
+        <div className='md:row-span-full'>
           <img className={`w-20 h-20 sm:w-35 sm:h-35`} src="/weekly.svg" alt="week" />
         </div>
         <div className='self-center text-3xl sm:text-5xl '>
           Apple Inc
         </div>
-        <div className=''>
-          <div className='text-3xl'>278.28</div>
-          <div>{"+ 0.25"} {"+ 0.09"}</div>
+        <div className={`${outlineClasses}`}>
+          <span className='text-3xl'>278.28</span>
+          <span>{"+ 0.25"}{"+ 0.09%"}</span>
         </div>
       </div>
     )
