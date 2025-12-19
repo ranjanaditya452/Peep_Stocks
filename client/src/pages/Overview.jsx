97f930aa3bbf104d99cabccbe03ecd6eca71e7b1
Aditya <ranjanaditya452@gmail.com>
2025-12-19 05:31:26 +0530

    Added more data ui outlets in overview

--- client/src/pages/Overview.jsx
+++ client/src/pages/Overview.jsx
@@ -9,6 +9,7 @@ const Overview = () => {
                     px-8 py-8 sm:px-16'>
       <OverviewMain />
       <div className='min-[500px]:w-3/4'><SearchBar /></div>
+      <CardHeader/>
       <DescriptionOverview/>
       <ChartData/>
     </div>
@@ -29,7 +30,7 @@ const OverviewMain = () => {
       <div className=''>
         <div className='text-2xl sm:text-4xl lg:text-7xl'>Apple Inc</div>
       </div>
-      <div className={`col-span-full pl-6 sm:pl-0 sm:col-span-1`}>
+      <div className={`col-span-full pl-4 sm:pl-0 sm:col-span-1`}>
         <div className='text-base md:text-2xl'>240.5 <span>-4.2</span> <span>-1.50%</span></div>
       </div>
     </div>
@@ -43,5 +44,30 @@ const DescriptionOverview = ()=>{
         </div>
   )
 }
+
+
+
+const CardHeader = () =>{
+
+  return(
+    <div className='flex flex-col gap-2'>
+      <div className='md:text-xl'>
+         <span>USA</span> • <span className='text-pink-400'>NASDAQ</span> • <span>USD</span>
+      </div>
+      <div className='flex gap-4'>
+        <div>
+          <h4>Market Cap</h4> <h4>$2.84T</h4>
+        </div>
+        <div >
+          <h4>P/E Ratio</h4> <h4>28.4</h4>
+        </div>
+        <div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
 
 export default Overview
