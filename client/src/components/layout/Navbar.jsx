bafd6876a4fe63e082c5aa541d3c3b3b3a964e74
Aditya <ranjanaditya452@gmail.com>
2025-12-20 05:31:07 +0530

    Removed searchbar to move it to header, made other changes to ui

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -3,17 +3,16 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-   <div className='sticky top-0 z-[9999] bg-white 
+   <div className='sticky top-0 z-9999 bg-white 
                    flex items-center justify-between 
                    px-4 py-3 pl-[7vw] h-16
                    w-full
                    md:px-16 md:h-20 md:text-xl
                    outline-2 outline-red-200'>
      <Link to="/">Peep-Stocks</Link>
-     <div className='px-6 ml-[5rem] hidden lg:flex justify-between gap-5'>
+     <div className='px-4 ml-20 hidden md:flex justify-between gap-2'>
       <Link to="/overview">Overview</Link>
       <div>Placeholder&nbsp;2</div>
-      <div>Placeholder&nbsp;3</div>
      </div>
      <div className='flex ml-auto gap-2'>
       <div className=''>Contact Me</div>
--- client/src/pages/Overview.jsx
+++ client/src/pages/Overview.jsx
@@ -8,10 +8,9 @@ const Overview = () => {
     <div className='flex flex-col gap-5
                     px-8 py-8 sm:px-16'>
       <OverviewMain />
-      <div className='min-[500px]:w-3/4'><SearchBar /></div>
-      <CardHeader/>
-      <DescriptionOverview/>
-      <ChartData/>
+      <CardHeader />
+      <DescriptionOverview />
+      <ChartData />
     </div>
   )
 }
@@ -21,53 +20,57 @@ const OverviewMain = () => {
   console.log(outlineClasses)
   return (
 
-    <div className={` grid grid-cols-[auto_1fr] 
-                        items-center gap-x-4 
-                        sm:grid-rows-2`}>
+    <div className='flex flex-col items-center gap-4
+                    sm:grid sm:grid-cols-[auto_1fr]  sm:gap-x-4 sm:grid-rows-2'>
       <div className='sm:row-span-full'>
-        <img className={`w-10 sm:w-20 lg:w-36`} src="/aapl.png" alt="week" />
+        <img className={`w-16 sm:w-20 lg:w-36`} src="/aapl.png" alt="week" />
       </div>
       <div className=''>
-        <div className='text-2xl sm:text-4xl lg:text-7xl'>Apple Inc</div>
+        <div className='text-3xl sm:text-4xl lg:text-7xl'>Apple Inc</div>
       </div>
-      <div className={`col-span-full pl-4 sm:pl-0 sm:col-span-1`}>
+      <div className={`col-span-full sm:pl-0 sm:col-span-1`}>
         <div className='text-base md:text-2xl'>240.5 <span>-4.2</span> <span>-1.50%</span></div>
       </div>
     </div>
   )
 }
 
-const DescriptionOverview = ()=>{
-  return(
-        <div className='text-sm md:text-base'>
-          Apple Inc. is a global tech giant designing & selling consumer electronics (iPhone, Mac, iPad, Watch), software (iOS, macOS), and online services (App Store, iCloud, Apple Music), known for innovation, seamless ecosystem integration, and strong brand.
-        </div>
-  )
-}
+const CardHeader = () => {
 
-
-
-const CardHeader = () =>{
-
-  return(
-    <div className='flex flex-col gap-2'>
-      <div className='md:text-xl'>
-         <span>USA</span> • <span className='text-pink-400'>NASDAQ</span> • <span>USD</span>
+  return (
+    <div className='flex flex-col items-center gap-2
+                    sm:items-start sm:flex-row
+                    text-xl md:text-2xl'>
+      <div className=''>
+        <span>USA</span> • <span className='text-pink-400'>NASDAQ</span> • <span>USD</span>
       </div>
-      <div className='flex gap-4'>
-        <div>
+      <div className='flex items-center gap-4 ml-4 sm:ml-6'>
+        <div className='flex flex-col gap-1 items-center sm:flex-row'>
           <h4>Market Cap</h4> <h4>$2.84T</h4>
         </div>
-        <div >
+        <div className='flex flex-col gap-1 items-center sm:flex-row'>
           <h4>P/E Ratio</h4> <h4>28.4</h4>
         </div>
         <div>
-
         </div>
       </div>
     </div>
   )
 }
 
+
+const DescriptionOverview = () => {
+  return (
+    <div className="text-sm md:text-base leading-relaxed text-slate-600 ">
+      <p>
+        Apple Inc. is a global technology company designing and selling consumer
+        electronics such as the iPhone, Mac, iPad, and Apple Watch.
+        It also develops software and services including iOS, macOS, the App Store,
+        iCloud, and Apple Music, and is known for its strong ecosystem and brand.
+      </p>
+    </div>
+
+  )
+}
 
 export default Overview
