78b64cccc1716bcfb4c053fdda56d3d2aa53ad2c
Aditya <ranjanaditya452@gmail.com>
2025-12-24 22:03:25 +0530

    Searchbar functionality finished

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   const [toggledSearchState, setToggledSearchState] = useState(false);
   const location = useLocation();
   const isHome = location.pathname === "/";
-  function toggleSearch(){
+  function toggleSearch() {
     setToggledSearchState(prev => !prev);
   }
 
@@ -23,14 +23,17 @@ const Navbar = () => {
         <div className='px-4  hidden lg:flex justify-between gap-2'>
           <Link to="/overview">Overview</Link>
           <div>Placeh&nbsp;2</div>
-          <div>Placeh&nbsp;3</div>
         </div>
+        {!isHome &&
+          <div className='hidden lg:block'>
+            <SearchBar />
+          </div>}
         {isHome ? null :
           <div className='relative'>
             <button onClick={toggleSearch} className='w-4 h-4 hover:cursor-pointer
                                                       flex items-center justify-center
                                                       relative 
-                                                      sm:w-6 sm:h-6 
+                                                      sm:w-6 sm:h-6 lg:hidden
                                                       outline-2 outline-gray-500 rounded-full'>
               <img src="/search.svg" alt="search" />
             </button></div>}
