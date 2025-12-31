2a299d3faea701ea06ef49c2ce6586ccc2f5a889
Aditya <ranjanaditya452@gmail.com>
2025-12-31 09:34:24 +0530

    Updated news page, create microcomponents for news Cards now

--- client/src/pages/StockNews.jsx
+++ client/src/pages/StockNews.jsx
@@ -6,8 +6,17 @@ const StockNews = () => {
     <div className='flex flex-col gap-5
                     px-8 py-8 sm:px-16'>
     <CompanyHeader/>
+    <h1 className='text-xl sm:text-2xl md:text-3xl'>NEWS</h1>
+    <NewsArea/>
     </div>
   )
+}
+
+const NewsArea = () => {
+  <div className='grid grid-cols-1
+                  sm:grid-cols-2 lg:grid-cols-3'>
+    
+  </div>
 }
 
 export default StockNews
