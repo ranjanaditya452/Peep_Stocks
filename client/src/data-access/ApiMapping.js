41074a74304bc7dd742f1199ac5653e1ebb96932
Aditya <ranjanaditya452@gmail.com>
2026-01-09 23:13:44 +0530

    Created a new mapper for api

---
+++ client/src/data-access/ApiMapping.js
@@ -0,0 +1,32 @@
+export function mapStockOverview(api)
+{   
+    if(!api || !api.symbol)
+    {
+        throw new Error("Invalid overview api response");
+    }
+    return {
+        symbol:api.symbol,
+        assetType:api.assetType,
+        name:api.name ?? "N/A",
+        description:api.description ?? "",
+        exchange:api.exchange ?? "",
+        currency:api.currency ?? "",
+        country:api.country ?? "",
+        sector:api.sector ?? "", 
+        officialSite:api.officialSite ?? "",
+        marketCap:Number(api.marketCap) || 0,
+        peRatio:Number(api.peRatio) || null,
+        fiftyTwoWeekHigh:Number(api.fiftyTwoWeekHigh) || null,
+        fiftyTwoWeekLow:Number(api.fiftyTwoWeekLow) || null,
+    }
+}
+
+export function mapStockStatus(api)
+{
+    return{
+        symbol:api.symbol,
+        price:api.price,
+        lastUpdated:api.lastUpdated,
+    }
+}
+      
