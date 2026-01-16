a6a9e74977be3dfab10223d3ed15b5067b0456e7
Aditya <ranjanaditya452@gmail.com>
2026-01-16 18:01:43 +0530

    Added a new fetch method to get news

--- client/src/data-access/StockQuerying.js
+++ client/src/data-access/StockQuerying.js
@@ -3,11 +3,7 @@ import { mapStockLogo, mapStockOverview, mapStockStatus } from "./ApiMapping";
 const BASE_URL = import.meta.env.VITE_API_BASE_URL ?? "http://localhost:8080";
 
 async function fetchJson(url) {
-    const res = await fetch(url,{
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });   
+    const res = await fetch(url);   
     if(!res.ok){
         throw new Error (`Request failed: ${res.status} ${res.statusText}`);
     }
@@ -27,6 +23,11 @@ export function fetchMonthlyStocks(symbol) {
     return fetchJson(`${BASE_URL}/api/v1/stocks/${encodeURIComponent(symbol)}/monthly`);
 }
 
+export function fetchLatestNews(symbol){
+    const params = new URLSearchParams({symbol});
+    return fetchJson(`${BASE_URL}/api/v1/news/latestNews?${params.toString()}`)
+}
+
 export async function fetchCompanyOverview(symbol){
     const apiOverviewResponse = await fetchJson(`${BASE_URL}/api/v1/stocks/overview-company/${encodeURIComponent(symbol)}`);
     return mapStockOverview(apiOverviewResponse);
@@ -42,3 +43,4 @@ export async function fetchStockStatus(symbol){
     return mapStockStatus(res);
     
 }
+
