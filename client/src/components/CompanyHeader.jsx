04c8ac3f96232a5604de07ffaf9d68dfe833ce62
Aditya <ranjanaditya452@gmail.com>
2026-01-16 19:56:18 +0530

    Added symbol from context instead of relying on another api response, cleaned up the ui

--- client/src/components/CompanyHeader.jsx
+++ client/src/components/CompanyHeader.jsx
@@ -1,39 +1,50 @@
 import React, { useContext, useEffect, useState } from 'react'
 import StockStatusContext from '../context/context-creation/StockStatusContext'
 import { fetchStockLogo } from '../data-access/StockQuerying';
+import StockContext from '../context/context-creation/StockContext';
 
-const CompanyHeader = ({name}) => {
-  const {stockStatus} = useContext(StockStatusContext);
-  
-  const[logoData,setLogoData] = useState(null);
-  const[error,setError] = useState(null);
-  const symbolStock = stockStatus?.symbol;
-  
+const CompanyHeader = ({ name }) => {
+  const { stockStatus } = useContext(StockStatusContext);
+  const {symbol} = useContext(StockContext);
 
-  useEffect(()=>{
+  const [logoData, setLogoData] = useState(null);
+  const [error, setError] = useState(null);
 
-    if (!symbolStock) return;
-    setError(null); 
+
+  useEffect(() => {
+
+    if (!symbol) return;
+    setError(null);
     setLogoData(null);
-       fetchStockLogo(symbolStock)
-       .then(setLogoData)
-       .catch(err => setError(err.message));
-    },[symbolStock]);
+    fetchStockLogo(symbol)
+      .then(setLogoData)
+      .catch(err => setError(err.message));
+  }, [symbol]);
 
-    if (!stockStatus) return null;
+  if (!stockStatus) return null;
 
-    return (
+  return (
     <div className='flex flex-col items-center gap-4
                     sm:grid sm:grid-cols-[auto_1fr]  sm:gap-x-4 sm:grid-rows-2'>
-      <div className='sm:row-span-full'>
-        {error ? <p>{error}</p> : logoData ?
-        ( <img className={`w-16 sm:w-20 lg:w-36`} 
-              src={logoData?.image} 
-              alt={stockStatus?.symbol ?? "Stock Image"} /> )
-          : ( <img className={`w-16 sm:w-20 lg:w-36`} 
-                   src="/circle_placeholder.svg" 
-                   alt= {"Stock Image"} /> )} 
+      <div className="sm:row-span-full
+                w-20 h-20
+                sm:w-24 sm:h-24
+                lg:w-40 lg:h-40
+                border-2 border-black
+                rounded-full
+                flex items-center justify-center">
+
+        {error ? (
+          <p>{error}</p>
+        ) : (
+          <img
+            className="w-3/4 h-3/4 object-contain"
+            src={logoData?.image ?? "/circle_placeholder.svg"}
+            alt={stockStatus?.symbol ?? "Stock Image"}
+          />
+        )}
       </div>
+
       <div>
         <div className='text-3xl sm:text-4xl lg:text-7xl'>{name ?? logoData?.name ?? stockStatus?.symbol}</div>
       </div>
